780715f4f232feb846878c94879ce895e578516a
hiveship <maelig.nantel@gmail.com>
2016-10-08 11:28:47 +0200

    RUN 1 OK

--- assets/js/run1.js
+++ assets/js/run1.js
@@ -3,13 +3,12 @@
 // CONSTANTES / VARIABLES DE JEU
 // =============================
 
-const PAS_ANGLE = 15;
+const PAS_ANGLE = 5;
 const DEBUG = true;
 
 // ----------------------- CONTRÔLE DU L'AFFICHAGE
 var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-var animation;
+var gamepad_connecte;
 
 // ----------------------- CONTRÔLE DU JEU
 var flag_finish = false;
@@ -29,7 +28,7 @@ var car = {
     orientation : 0,
     vitesse : 1
 };
-car.image.src = "police.png";
+car.image.src = "assets/images/police.png";
 car.x = car.image.width;
 car.y = car.image.height;
 
@@ -40,7 +39,7 @@ var car2 = {
     angle : 0,
     vitesse : 1
 };
-car2.image.src = "lambo.png";
+car2.image.src = "assets/images/lambo.png";
 car2.x = canvas.height / 2;
 car2.y = canvas.width / 2;
 
@@ -55,41 +54,38 @@ function mainLoop() {
     update();
     draw();
     if ( !flag_finish ) {
-        animation = requestAnimationFrame(mainLoop);
-    } else {
-        cancelAnimationFrame(animation);
-        if (DEBUG) {
-            console.log("Fin de requestAnimationFrame");
-        }
+        requestAnimationFrame(mainLoop);
     }
 }
 
 /**
  * Met à jour la voiture.
- * La vitesse peut varier entre '1' et '10', '10' étant le plus rapide.
+ * La vitesse peut varier entre '1' et '5', '5' étant le plus rapide.
  * A chaque demande, la voiture fera une rotation de 'PAS_ANGLE' degrés.
  * La nouvelle position de la voiture est fonction de son orientation et de sa vitesse.
  */
 function update() {
-    if (flag_accelerer) {
+    verifierGamepad();
+
+    if ( flag_accelerer ) {
         car.vitesse++;
-        if (car.vitesse > 10) {
-            car.vitesse = 10;
+        if ( car.vitesse > 5 ) {
+            car.vitesse = 5;
         }
         flag_accelerer = false;
     }
-    if (flag_ralentir) {
+    if ( flag_ralentir ) {
         car.vitesse--;
-        if (car.vitesse < 1) {
+        if ( car.vitesse < 1 ) {
             car.vitesse = 1;
         }
         flag_ralentir = false;
     }
-    if (flag_droite) {
+    if ( flag_droite ) {
         car.orientation = car.orientation + PAS_ANGLE;
         flag_droite = false;
     }
-    if (flag_gauche){
+    if ( flag_gauche ) {
         car.orientation = car.orientation - PAS_ANGLE;
         flag_gauche = false;
     }
@@ -98,7 +94,7 @@ function update() {
     car.x += car.vitesse * Math.cos(Math.PI / 180 * car.orientation);
     car.y += car.vitesse * Math.sin(Math.PI / 180 * car.orientation);
 
-    if ( colissionBords() || colision(car,car2) ) {
+    if ( colissionBords() || colision(car, car2) ) {
         flag_finish = true;
     }
 }
@@ -139,7 +135,7 @@ function colissionBords() {
 function colision(A, B) {
     if ( A.x < B.x + B.image.width && A.x + A.image.width > B.x &&
         A.y < B.y + B.image.height && A.y + A.image.height > B.y ) {
-        if (DEBUG) {
+        if ( DEBUG ) {
             console.log("Colision entre deux véhicules");
         }
         return true;
@@ -147,8 +143,11 @@ function colision(A, B) {
     return false;
 }
 
-function draw() { // Gère l'affichage de la voiture en fonction de sa position
-    context = canvas.getContext("2d");
+/**
+ * Affiche les éléments du jeu en fonction de leurs coordonées
+ */
+function draw() {
+    let context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height); // Efface les affichages précédents
 
     context.save();
@@ -171,10 +170,11 @@ function draw() { // Gère l'affichage de la voiture en fonction de sa position
 
 /**
  * Action déclanchée lors d'une intéraction avec un utilisateur de type : touche/bouton appuyé.
+ * Ne prends pas en compte les actions sur le gamepad, ce dernier n'envoyant pas d'event.
  * @param event
  */
 function keypress_handler(event) {
-    if (DEBUG) {
+    if ( DEBUG ) {
         console.log("Touche appuyée : " + event.keyCode);
     }
     switch ( event.keyCode ) {
@@ -184,15 +184,70 @@ function keypress_handler(event) {
         case 39: // Flèche droite
             flag_droite = true;
             break;
-        case 38: // haut
+        case 38: // Flèche haute
             flag_accelerer = true;
             break;
-        case 40: // bas
+        case 40: // Flèche basse
             flag_ralentir = true;
             break;
-    } //TODO: Ajouter les cas pour le gamepad
+    }
 }
 
-window.addEventListener("keydown", keypress_handler, false);
-// Premier lancement
-animation = requestAnimationFrame(mainLoop);
+// ===================
+// GESTIION DU GAMEPAD
+// ===================
+
+/**
+ * Taite les actions sur les boutons '1', '2', '3' et '4' du gamepad.
+ * Il faut appeler cette fonction dans la boucle de jeu car il n'y a pas d'event handler associé au gamepad.
+ * FIXME: Sur mon poste, les actions sur les flèches du gamepad ne sont pas détectées.
+ */
+function verifierGamepad() {
+    if ( gamepad_connecte ) {
+        let gamepad = navigator.getGamepads()[ 0 ]; // On suppose que le navigateur est assez récent...
+        if ( gamepad.buttons[ 0 ].pressed ) {
+            flag_accelerer = true;
+        }
+        if ( gamepad.buttons[ 2 ].pressed ) {
+            flag_ralentir = 1;
+        }
+        if ( gamepad.buttons[ 3 ].pressed ) {
+            flag_gauche = true;
+        }
+        if ( gamepad.buttons[ 1 ].pressed ) {
+            flag_droite = true;
+        }
+    }
+}
+
+// ====
+// MAIN
+// ====
+
+$(document).ready(function () {
+    $(window).on("gamepadconnected", function () {
+        gamepad_connecte = true;
+        $("#gamepadPrompt").text("Gamepad connecté ! Appuuyer sur une touche '1', '2', '3' ou '4' pour commencer !");
+        if ( DEBUG ) {
+            console.log("Gamepad connecté");
+        }
+    });
+
+    $(window).on("gamepaddisconnected", function () {//récupération de l'événement (cf plus bas)
+        gamepad_connecte = false;
+        $("#gamepadPrompt").text("Gamepad connecté ! Appuuyer sur une touche '1', '2', '3' ou '4' pour commencer !");
+        if ( DEBUG ) {
+            console.log("Gamepad connecté");
+        }
+    });
+
+    // Autorise le contrôle via clavier uniquement en développement/debug
+    if ( DEBUG ) {
+        window.addEventListener("keydown", keypress_handler, false);
+    }
+
+    if ( navigator.getGamepads()[ 0 ] ) { // Regarde si un gamepad est déjà branché avant l'éxécution du JavaScript
+        gamepad_connecte = true;
+    }
+    requestAnimationFrame(mainLoop);
+});
